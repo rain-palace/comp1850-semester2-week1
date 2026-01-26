b2236198232c143de3bb3ba8f935a12c62bab73e
rain-palace <alex.xinyichen@gmail.com>
2026-01-26 12:54:57 +0000

    end of session 1

--- session_1/1_country/task1.sql
+++ session_1/1_country/task1.sql
@@ -12,4 +12,13 @@
 
 -- write your sql code here
 
-SELECT * from countries;
+--SELECT * FROM countries;
+--SELECT Country, Population FROM countries;
+--SELECT Country FROM countries WHERE continent='Europe';
+--SELECT Country, Population FROM countries WHERE Population>100000000;
+--SELECT Country, GDPPerCapita FROM countries WHERE continent='Asia' AND GDPPerCapita>20000;
+--SELECT Country, LiteracyPercent FROM countries WHERE LiteracyPercent<70;
+--SELECT Country FROM countries WHERE CoastlineRatio=0;
+--SELECT Country FROM countries WHERE Country LIKE 'A%';
+--SELECT Country FROM countries WHERE Country LIKE '%United%';
+--SELECT Country, Birthrate, Deathrate, Population FROM countries WHERE Birthrate>Deathrate AND Population>50000000;
--- session_1/1_country/task2.sql
+++ session_1/1_country/task2.sql
@@ -12,3 +12,13 @@
 
 -- write your sql code here
 
+--SELECT Country, Population FROM countries ORDER BY Population DESC LIMIT 10;
+--SELECT Country, AreaSqMi FROM countries ORDER BY AreaSqMi ASC LIMIT 5;
+--SELECT Country FROM countries WHERE Continent='Europe' ORDER BY AreaSqMi DESC LIMIT 1;
+--SELECT Country FROM countries WHERE Continent='Africa' ORDER BY AreaSqMi ASC LIMIT 3;
+--SELECT Country FROM countries ORDER BY LiteracyPercent LIMIT 5;
+--SELECT Country FROM countries WHERE Country LIKE 'C%' ORDER BY GDPPerCapita LIMIT 3;
+--SELECT Country FROM countries WHERE InfantMortalityPer1000>50 ORDER BY LiteracyPercent DESC LIMIT 1;
+--SELECT Country FROM countries WHERE GDPPerCapita>10000 ORDER BY PhonesPer1000 LIMIT 5;
+--SELECT Country FROM countries WHERE Continent='South America' AND CoastlineRatio=0 ORDER BY GDPPerCapita LIMIT 1;
+--SELECT Country FROM countries ORDER BY Country DESC LIMIT 10;

c444e0dc94f607304beef2f476f200e07efc60e4
rain-palace <alex.xinyichen@gmail.com>
2026-01-30 10:52:57 +0000

    end of session 2

--- session_2/2_joins/tasks.sql
+++ session_2/2_joins/tasks.sql
@@ -7,3 +7,10 @@
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
 
+--SELECT content FROM posts WHERE deleted==1;
+--SELECT username FROM users WHERE username LIKE 'a%';
+--SELECT content, username FROM users JOIN posts ON users.id=posts.user_id WHERE content LIKE '%small%';
+--SELECT content, username, posts.created_on FROM users JOIN posts ON users.id=posts.user_id WHERE deleted==0 ORDER BY posts.created_on;
+--SELECT username, COUNT(content) AS NumberOfPosts FROM users JOIN posts ON users.id=posts.user_id GROUP BY username;
+--SELECT username, COUNT(content) AS NumberOfPosts FROM users JOIN posts ON users.id=posts.user_id GROUP BY username HAVING NumberOfPosts>3;
+SELECT username, COUNT(following_user_id) AS NumberOfFollows From follows join users ON follows.following_user_id=users.id GROUP BY username;
